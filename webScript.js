2ea5bdcfd6878b507199b5260f5c85614cfe983a
ReceptenVanOns <130905908+ReceptenVanOns@users.noreply.github.com>
2023-04-16 15:17:02 +0200

    Update webScript.js
    fixed link mistake _ to space

--- webScript.js
+++ webScript.js
@@ -102,7 +102,7 @@ function displayJsonToHtmlTable(jsonData){
             htmlData += '<tr>';
             htmlData += '<td><a class="receptLink" id="receptLink' + jsonCounter + '" href="'+row['Waar te vinden']+'" target="_blank"><img src="' + row['Foto'] + '"></a></td>'; 
             htmlData += '<td class="receptDet">';
-            htmlData += '<p class="receptTitle" onclick="setCookie(' + jsonCounter +');window.open(\'Bekijk_Recept.html\', \'_blank\');">' + row['Naam recept'] + '</p>';
+            htmlData += '<p class="receptTitle" onclick="setCookie(' + jsonCounter +');window.open(\'Bekijk Recept.html\', \'_blank\');">' + row['Naam recept'] + '</p>';
             htmlData += '<p>Duur: <span class="recDuurId">' + row["Duur"] + '</span>Soort: <span class="recSoortId">' + row["Soort"] + '</span>Keuken: <span class="recKeukenId">' + row["Keuken"] + '</span></p>';
             htmlData += '</td></tr>';
             //}
@@ -256,7 +256,7 @@ function search() {
             htmlData += '<tr>';
             htmlData += '<td><a class="receptLink" id="receptLink' + jsonCounter + '" href="'+row['Waar te vinden']+'" target="_blank"><img src="' + row['Foto'] + '"></a></td>'; 
             htmlData += '<td class="receptDet">';
-            htmlData += '<p class="receptTitle" onclick="setCookie(' + jsonCounter +');window.open(\'Bekijk_Recept.html\', \'_blank\');">' + row['Naam recept'] + '</p>';
+            htmlData += '<p class="receptTitle" onclick="setCookie(' + jsonCounter +');window.open(\'Bekijk Recept.html\', \'_blank\');">' + row['Naam recept'] + '</p>';
             //htmlData += '<p>Duur: ' + row["Duur"] + '<br>Soort: ' + row["Soort"] + '<br>Keuken: ' + row["Keuken"] + '</p>';
             htmlData += '<p>Duur: <span class="recDuurId">' + row["Duur"] + '</span>Soort: <span class="recSoortId">' + row["Soort"] + '</span>Keuken: <span class="recKeukenId">' + row["Keuken"] + '</span></p>';
             htmlData += '</td><td class="receptButton"><input onclick="addOrRemoveRecept(' + jsonCounter + ')" type="checkbox" class="check" id="addRemoveButton"';
