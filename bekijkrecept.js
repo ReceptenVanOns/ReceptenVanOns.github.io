a397da90c4e2fb9365711c881f8f66848d045a5b
ReceptenVanOns <130905908+ReceptenVanOns@users.noreply.github.com>
2023-04-17 21:06:27 +0200

    Update bekijkrecept.js
    Added lots of content

--- bekijkrecept.js
+++ bekijkrecept.js
@@ -8,8 +8,6 @@ function bekijkReceptenPaginaLaden() {
 }
 
 function getReceptOmTeBekijken() {
-    console.log(document.cookie);
-    console.log(document.cookie.split("=")[1]);
     console.log(JSON.parse(document.cookie.split("=")[1]));
     let row = JSON.parse(document.cookie.split("=")[1]);
     return row;
@@ -23,11 +21,21 @@ function bekijkReceptenTitelLaden(row) {
 }
 
 function bekijkReceptenDetailsLaden(row) {
-    //console.log("set details");
-    //console.log(row);
-    //console.log(row['Gezond']);
     var bekRecDetails = document.getElementById("ReceptenDetails");
-    bekRecDetails.innerHTML = row['Gezond'];
+    var keywords = getKeywords(row);
+    var htmlCode = "<p>Recept keywords:";
+    for (var i=0; i<keywords.length; i++) {
+        htmlCode += " <span>" + keywords[i] + "</span> ";
+    }
+    htmlCode+= "</p><br>";
+
+    htmlCode+= "<p>Bereiding:</p>";
+    var bereidingList = row['Bereiding'].split("][");
+    for (var i=0; i<bereidingList.length; i++) {
+        htmlCode+= "<p>" + bereidingList[i] + "</p>";
+    }
+
+    bekRecDetails.innerHTML = htmlCode;
     //console.log("done details");
 }
 
@@ -41,6 +49,40 @@ function bekijkReceptenFotoLaden(row) {
 function bekijkReceptenIngredientenLaden(row) {
     //console.log("set ingredienten");
     var bekRecIngredienten = document.getElementById("ReceptenIngredienten");
-    bekRecIngredienten.innerHTML = row['Ingredienten met aantallen'];
+    var ingredientenRecept = row['Ingredienten met aantallen'];
+    var ingredientenReceptList = ingredientenRecept.split(",");
+    var htmlCode = "<p>Ingredienten</p><table id='ingredientenTabel'>";
+    for (var i=0; i<ingredientenReceptList.length; i++) {
+        var ingredientMetAantal = ingredientenReceptList[i];
+        htmlCode += '<tr><td>' + ingredientMetAantal.split(":")[0] + '</td><td>' + ingredientMetAantal.split(":")[1] + "</td></tr>";
+    }
+    bekRecIngredienten.innerHTML = htmlCode;
     //console.log("done ingredienten");
+}
+
+function getKeywords(row) {
+    var keywordsList = [];
+    if (row['Gezond'] == "Gezond") {
+        keywordsList.push("Gezond");
+    }
+    if (row['Budget'] == "Ja") {
+        keywordsList.push("Budget");
+    }
+    if (row['Alcohol'] == "Ja") {
+        keywordsList.push("Alcohol");
+    }
+    keywordsList.push(row['Vlees of']);
+    if (row['Soort'] != "Onbekend") {
+        keywordsList.push(row['Soort']);
+    } 
+    if (row['Keuken'] != "Niet specifiek") {
+        keywordsList.push(row['Keuken']);
+    } 
+    if (row['Oven / frituur / wok'] != "Niet specifiek") {
+        keywordsList.push(row['Oven / frituur / wok']);
+    } 
+    if (row['Seizoen'] != "All-Round") {
+        keywordsList.push(row['Seizoen']);
+    } 
+    return keywordsList;
 }
