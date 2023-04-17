c1adb84d3642bb7c934112a50ecbfa2706b4ca4c
ReceptenVanOns <130905908+ReceptenVanOns@users.noreply.github.com>
2023-04-17 18:34:12 +0200

    Update webScript.js
    fixed jsonCounter change mistake

--- webScript.js
+++ webScript.js
@@ -100,9 +100,9 @@ function displayJsonToHtmlTable(jsonData){
                 //             + '<button class="persButton" onclick="changePersonen(' + jsonCounter + ', true)">+</button></div>';
                 // test_data += '</div></div>';
             htmlData += '<tr>';
-            htmlData += '<td><a class="receptLink" id="receptLink' + (jsonCounter-1) + '" href="'+row['Waar te vinden']+'" target="_blank"><img src="' + row['Foto'] + '"></a></td>'; 
+            htmlData += '<td><a class="receptLink" id="receptLink' + jsonCounter + '" href="'+row['Waar te vinden']+'" target="_blank"><img src="' + row['Foto'] + '"></a></td>'; 
             htmlData += '<td class="receptDet">';
-            htmlData += '<p class="receptTitle" onclick="setCookie(' + jsonCounter +');window.open(\'Bekijk Recept.html\', \'_blank\');">' + row['Naam recept'] + '</p>';
+            htmlData += '<p class="receptTitle" onclick="setCookie(' + (jsonCounter-1) +');window.open(\'Bekijk Recept.html\', \'_blank\');">' + row['Naam recept'] + '</p>';
             htmlData += '<p>Duur: <span class="recDuurId">' + row["Duur"] + '</span>Soort: <span class="recSoortId">' + row["Soort"] + '</span>Keuken: <span class="recKeukenId">' + row["Keuken"] + '</span></p>';
             htmlData += '</td></tr>';
             //}
@@ -959,6 +959,7 @@ function setCookie(receptID) {
     const d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
     let expires = "expires="+d.toUTCString();
+    console.log(JSON.stringify(jsonData[receptID]));
     document.cookie = "jsonData=" + JSON.stringify(jsonData[receptID]) + "; expires=" + expires + "; path=/";
 }
 
